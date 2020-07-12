4873e6a74a4cb3debdc7497dfc530f3955a962c3
Navneet Singh <navneetsingh6122@gmail.com>
2020-07-11 21:10:47 -0400

    Navneet Singh

--- app/src/main/java/com/example/cupocoffee/InstructionActivity.java
+++ app/src/main/java/com/example/cupocoffee/InstructionActivity.java
@@ -2,13 +2,24 @@ package com.example.cupocoffee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InstructionActivity extends AppCompatActivity {
-
+Button menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instruction);
+        menu = (Button)findViewById(R.id.button_menu);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InstructionActivity.this , MenuPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
