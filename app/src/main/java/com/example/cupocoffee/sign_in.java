b4d57d0e5cbfcc445584b7615bcced1d641a05a3
vishav jain <vishavjain27@gmail.com>
2020-07-11 21:01:07 -0400

    Vishav Jain

--- app/src/main/java/com/example/cupocoffee/sign_in.java
+++ app/src/main/java/com/example/cupocoffee/sign_in.java
@@ -2,13 +2,26 @@ package com.example.cupocoffee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class sign_in extends AppCompatActivity {
-
+Button instruction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        instruction = (Button)findViewById(R.id.button_login);
+
+        instruction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(sign_in.this , InstructionActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
