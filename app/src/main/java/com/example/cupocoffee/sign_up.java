a9dca8a9d5a248ae0c8087e645a742b4d14123a4
Jobanjeet <jobanjeetsg99@gmail.com>
2020-07-11 20:52:54 -0400

    Jobanjeet Singh

--- app/src/main/java/com/example/cupocoffee/sign_up.java
+++ app/src/main/java/com/example/cupocoffee/sign_up.java
@@ -2,13 +2,26 @@ package com.example.cupocoffee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class sign_up extends AppCompatActivity {
+    TextView signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        signin  = (TextView)findViewById(R.id.login);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(sign_up.this , sign_in.class);
+                startActivity(intent);
+            }
+        });
     }
 }
