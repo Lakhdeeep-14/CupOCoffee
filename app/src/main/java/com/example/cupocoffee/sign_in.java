f85005a1008a7175a55949a9e011bdf2d6a7f7de
Navneet Singh <navneetsingh6122@gmail.com>
2020-09-12 11:53:13 -0400

    Node Js With mongodb and express

--- app/src/main/java/com/example/cupocoffee/sign_in.java
+++ app/src/main/java/com/example/cupocoffee/sign_in.java
@@ -6,20 +6,74 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class sign_in extends AppCompatActivity {
 Button instruction;
+
+private Retrofit retrofit;
+private RetrofitInterface retrofitInterface;
+private String BASE_URL = "http://10.0.2.2:3000";
+
+EditText emailEdit;
+EditText passwordEdit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        retrofitInterface = retrofit.create(RetrofitInterface.class);
+
         setContentView(R.layout.activity_sign_in);
         instruction = (Button)findViewById(R.id.button_login);
+
+       emailEdit = (EditText)findViewById(R.id.signin_email);
+       passwordEdit = (EditText)findViewById(R.id.signin_password);
+
 
         instruction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(sign_in.this , InstructionActivity.class);
+
+                HashMap<String , String >  map = new HashMap<>();
+
+                map.put("email" ,emailEdit.getText().toString());
+                map.put("password" , passwordEdit.getText().toString());
+
+                Call<LoginResult> call = retrofitInterface.executeLogin(map);
+
+                call.enqueue(new Callback<LoginResult>() {
+                    @Override
+                    public void onResponse(Call<LoginResult> call, Response<LoginResult> response) {
+                        if(response.code()==200){
+                               Intent intent = new Intent(sign_in.this , InstructionActivity.class);
                 startActivity(intent);
+                        }else if(response.code()==404){
+                            Toast.makeText(sign_in.this , "Invalid Credentials" , Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<LoginResult> call, Throwable t) {
+                        Toast.makeText(sign_in.this , t.getMessage() , Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
             }
         });
 
--- app/src/main/java/com/example/cupocoffee/sign_up.java
+++ app/src/main/java/com/example/cupocoffee/sign_up.java
@@ -5,16 +5,83 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class sign_up extends AppCompatActivity {
     TextView signin;
+    Button register;
+    EditText email;
+    EditText name;
+    EditText password;
+    private Retrofit retrofit;
+    private RetrofitInterface retrofitInterface;
+    private String BASE_URL = "http://10.0.2.2:3000";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         signin  = (TextView)findViewById(R.id.login);
+
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        retrofitInterface = retrofit.create(RetrofitInterface.class);
+
+        register= (Button)findViewById(R.id.button_register);
+        email = (EditText)findViewById(R.id.register_email);
+        name = (EditText)findViewById(R.id.username);
+        password= (EditText)findViewById(R.id.register_password);
+
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HashMap<String , String> map = new HashMap<>();
+
+                map.put("name" , name.getText().toString());
+                map.put("email" ,email.getText().toString());
+                map.put("password" , password.getText().toString());
+
+                Call<Void> call = retrofitInterface.executeSignUp(map);
+
+                call.enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        if(response.code()==200){
+                            Toast.makeText(sign_up.this, "Sign Up successfully", Toast.LENGTH_SHORT).show();
+                        }else if(response.code()==400) {
+                            Toast.makeText(sign_up.this , "Already registered" , Toast.LENGTH_SHORT).show();
+
+                        }
+                        }
+
+
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                        Toast.makeText(sign_up.this , t.getMessage() , Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+            }
+        });
 
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
