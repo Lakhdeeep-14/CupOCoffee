9fd5470d1e8602de2ae2008bfb8528e731a53f4d
vishav jain <vishavjain27@gmail.com>
2020-09-12 17:46:53 -0400

    Sidebar Navigation with page Linking

---
+++ app/src/main/java/com/example/cupocoffee/MainActivity2.java
@@ -0,0 +1,24 @@
+package com.example.cupocoffee;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/cupocoffee/ProfileActivity.java
@@ -0,0 +1,53 @@
+package com.example.cupocoffee;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class ProfileActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.bringToFront();
+
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.profile);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.home:
+                Intent intent = new Intent(this, MenuPage.class);
+                startActivity(intent);
+                break;
+            case R.id.profile:
+                Intent profile = new Intent(this, ProfileActivity.class);
+                startActivity(profile);
+                break;
+            case R.id.contactUs:
+                Intent contact = new Intent(this, ContactUsActivity.class);
+                startActivity(contact);
+                break;
+        }
+        return true;
+
+    }
+}
