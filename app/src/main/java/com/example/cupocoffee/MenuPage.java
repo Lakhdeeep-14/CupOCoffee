e2683490f2383502f24e417aaa6b9dd02e8e0cfb
Gurjivan <gurjivan177@gmail.com>
2020-09-13 03:48:43 -0400

    new updates

--- app/src/main/java/com/example/cupocoffee/MenuPage.java
+++ app/src/main/java/com/example/cupocoffee/MenuPage.java
@@ -23,20 +23,30 @@ public class MenuPage extends AppCompatActivity implements NavigationView.OnNavi
     int hotCount;
     int chaiCount;
     int icedCount;
+    int hotCount1;
+    int hotCount2;
 
     Button inc_hot;
+    Button inc_hot1;
+    Button inc_hot2;
     Button inc_chai;
     Button inc_iced;
 
     Button dec_hot;
+    Button dec_hot1;
+    Button dec_hot2;
     Button dec_chai;
     Button dec_iced;
 
     TextView Qua_hot;
+    TextView Qua_hot1;
+    TextView Qua_hot2;
     TextView Qua_chai;
     TextView Qua_iced;
 
     float hot_price;
+    float hot_price1;
+    float hot_price2;
     float chai_price;
     float iced_price;
     float totalMoney;
@@ -57,17 +67,22 @@ navigationView.setCheckedItem(R.id.home);
 
         inc_hot = (Button) findViewById(R.id.increaseQuantity_hot);
         dec_hot = (Button)findViewById(R.id.decreaseQuantity_hot);
-
         Qua_hot = (TextView)findViewById(R.id.Quantity_hot);
+
+        inc_hot1 = (Button) findViewById(R.id.increaseQuantity_hot1);
+        dec_hot1 = (Button)findViewById(R.id.decreaseQuantity_hot1);
+        Qua_hot1 = (TextView)findViewById(R.id.Quantity_hot1);
+
+        inc_hot2 = (Button) findViewById(R.id.increaseQuantity_hot2);
+        dec_hot2 = (Button)findViewById(R.id.decreaseQuantity_hot2);
+        Qua_hot2 = (TextView)findViewById(R.id.Quantity_hot2);
 
         inc_chai = (Button)findViewById(R.id.increaseQuantity_chai);
         dec_chai = (Button)findViewById(R.id.decreaseQuantity_chai);
-
         Qua_chai = (TextView)findViewById(R.id.Quantity_chai);
 
         inc_iced = (Button)findViewById(R.id.increaseQuantity_iced);
         dec_iced = (Button)findViewById(R.id.decreaseQuantity_iced);
-
         Qua_iced = (TextView) findViewById(R.id.Quantity_iced);
 
 
@@ -89,7 +104,47 @@ navigationView.setCheckedItem(R.id.home);
                 if(hotCount>0)
                     hotCount--;
                 Qua_hot.setText(Integer.toString(hotCount));
-                hot_price = (float)(hot_price*7.99);
+                hot_price = (float)(hot_price*8.99);
+            }
+        });
+
+        inc_hot1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hotCount1++;
+                Qua_hot1.setText(Integer.toString(hotCount1));
+                hot_price1 = (float) (hotCount1*8.99);
+            }
+        });
+
+
+        dec_hot1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(hotCount1>0)
+                    hotCount1--;
+                Qua_hot1.setText(Integer.toString(hotCount1));
+                hot_price1 = (float)(hot_price1*8.99);
+            }
+        });
+
+        inc_hot2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hotCount2++;
+                Qua_hot2.setText(Integer.toString(hotCount2));
+                hot_price2 = (float) (hotCount2*9.99);
+            }
+        });
+
+
+        dec_hot2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(hotCount2>0)
+                    hotCount2--;
+                Qua_hot2.setText(Integer.toString(hotCount2));
+                hot_price2 = (float)(hot_price2*9.99);
             }
         });
 
@@ -137,7 +192,7 @@ navigationView.setCheckedItem(R.id.home);
         Checkout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                totalMoney=(float)(hot_price+chai_price+iced_price);
+                totalMoney=(float)(hot_price+hot_price1+hot_price2+chai_price+iced_price);
                 Intent intent = new Intent(MenuPage.this,CheckoutActivity.class);
                 intent.putExtra("value",totalMoney);
                 startActivity(intent);
